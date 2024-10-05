b13d94e15b53ccdb899b498f732f4e00d1945d22
Gabriel Prevosto <prevosto.gabriel@neuf.fr>
2024-10-05 10:59:58 +0200

    Fix internal links with vueRouterMode: 'history'

--- linepart/quasar.config.js
+++ linepart/quasar.config.js
@@ -33,8 +33,8 @@ module.exports = configure(function (ctx) {
     // --> boot files are part of "main.js"
     // https://v2.quasar.dev/quasar-cli-webpack/boot-files
     boot: [
-      
-      
+
+
     ],
 
     // https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js#Property%3A-css
@@ -58,7 +58,7 @@ module.exports = configure(function (ctx) {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js#Property%3A-build
     build: {
-      vueRouterMode: 'hash', // available values: 'hash', 'history'
+      vueRouterMode: 'history', // available values: 'hash', 'history'
 
       // transpile: false,
       // publicPath: '/',
